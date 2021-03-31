ef228259188ac393333800683d6b3b2cf91238f2
S3Prototype <liuqahs15@gmail.com>
2021-03-31 16:47:37 -0400

    Ugh

--- src/server.js
+++ src/server.js
@@ -1,17 +1,11 @@
 const express = require('express')
 const app = express()
-const imagesHandler = require('./imagesHandler')
-const bodyParser = require('body-parser')
-const fs = require('fs');
-const multer = require('multer')
-const upload = multer({dest: '/saved_images'})
 const writeHandler = require('./write')
 const cors = require('cors')
 
 app.use(cors())
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
-app.use('/api/images', imagesHandler)
 
 app.use('/api/write', writeHandler)
 
@@ -19,6 +13,6 @@ app.get('/', function (req, res) {
     res.send('Hello World')
 }) 
 // console.log('Port is', process.env.PORT)
-var port = process.env.PORT || '0.0.0.0';
-// var port = process.env.PORT || '2100';
+// var port = process.env.PORT || '0.0.0.0';
+var port = process.env.PORT || '2100';
 app.listen(port)
